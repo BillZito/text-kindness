aef6fa57f3eea855debf2c05632d7f16e9146a4b
Billzito <billzito@billzito-Inspiron-5520.(none)>
2015-08-15 22:00:29 -0400

    didn't actually edit the files other than spaces

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --log-file=-
+web: gunicorn text_kind:app --log-file=-
---
+++ text_kind.py
@@ -0,0 +1,21 @@
+"""
+From Twilio https://www.twilio.com/docs/quickstart/python/sms/hello-monkey
+should allow us to send replies to texts 
+saving as a new thing
+"""
+
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
