21b2a46f3d78d92bfa56f3512c9d43081a679394
Billzito <billzito@billzito-Inspiron-5520.(none)>
2015-08-26 16:54:33 -0400

    added ability to personalize daily messages

--- manage.py
+++ manage.py
@@ -2,14 +2,67 @@ from flask import Flask, request, redirect
 from flask.ext.script import Manager
 from flask.ext.script import Command
 from twilio.rest import TwilioRestClient
+import twilio.twiml
+import random
 
 app = Flask(__name__)
 
 manager = Manager(app)
  
+#list of numbers to look up upon receiving message
+callers = {
+    "+14158675309": "Curious George",
+    "+14103532508": "BillZ",
+    "+14109910925": "Mellie",
+    "+14432547513": "BillC",
+    "+15206098877": "Olivia",
+    "+13019436377": "Hannah",
+    "+14138414718": "Alice",
+    "+14103530094": "Jenny",
+}
+
+#account sid and token
 account_sid = "AC391d717b317d85ec6bc4278575769ab2"
 auth_token = "e306bcddeef2de9a43a7fac69c835eec"
 
+#send morning message
+class Send_morning(Command):
+	"send a message in the morning"
+
+	def run(self):
+		#identify their number
+    		from_number = "+14103532508"
+    		
+	        #if number in our directory above, pick out their name and add it to message string to be sent below
+    		if from_number in callers:
+        		name = callers[from_number]
+    
+	        #otherwise, consider them a monkey and respond accordingly
+    		else:
+        		name = "Monkey" 
+
+
+	        #open and write text to list--should let it throw exception, should let it be personalized based on user
+    		#initalize list
+    		still_happy = []
+    
+                #initalize file and then fill the list with the file contents
+    		new_file_name = name + ".txt"
+    		file = open(new_file_name, 'r')    
+    		num_lines = 0
+
+    		for line in file:
+        		still_happy.append(line)
+        		num_lines += 1
+
+		client = TwilioRestClient(account_sid, auth_token)
+                
+		for x in (0, 2):
+			message = client.messages.create(to="+14103532508", from_="+14437753700", 
+					body=still_happy[random.randint(0, num_lines-1)])
+
+
+#test of text capability
 class Send(Command):
 	"send a message to myself"
 	
@@ -18,6 +71,7 @@ class Send(Command):
  		message = client.messages.create(to="+14103532508", from_="+14437753700",
                                      body="Hello there!")
 
+#test of any capability
 class Hello(Command):
         "prints hello world"
 
@@ -27,6 +81,7 @@ class Hello(Command):
 
 manager.add_command('hello', Hello())
 manager.add_command('send', Send())
+manager.add_command('send_morning', Send_morning())
 
 if __name__ == "__main__":
 	manager.run()
